644fdc5d840c67338dd349b4c5115fe3882d43a6
vinoth <itsvinoth59@gmail.com>
2018-06-21 00:33:50 -0500

    First test commit

---
+++ src/pages/layout/Header.vue
@@ -0,0 +1,14 @@
+<template>
+	<div id="sample">
+		sample header will come here
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+<style scoped>
+</style>
+
---
+++ src/pages/layout/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+	<appheader></appheader>
+</template>
+<script>
+import Header from './Header.vue' 
+export default
+{
+	name:'Layout',
+	components:{
+		'appheader':Header
+	}
+}
+</script>
+
+<style scoped>
+</style>
