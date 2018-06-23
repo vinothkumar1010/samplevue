549d2df556cbce0c0fb57b505a2d87bcb072708b
vinoth <itsvinoth59@gmail.com>
2018-06-23 14:24:42 -0500

    files added for tracking

---
+++ src/pages/errorpages/NotFound.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="notFoundHolder">
+        <div id="notFound">
+            <img :src="require('../../assets/notfound.jpg')">
+        </div>
+        <div id="notFoundText">
+            Developers went to brew some coffee !! Hold on a moment !! They will come back and fix the issue !!
+        </div>
+    </div>
+</template>
+<script>
+    export default
+    {
+        name:'NotFound'
+    }
+</script>
+<style scoped lang="scss">
+#notFoundHolder
+{
+    display:block;
+    
+   div
+    {
+        display:inline-block;
+        width:49%;
+        vertical-align:top;
+        color:#1AB7EA;
+    }   
+}
+</style>
---
+++ src/pages/layout/Navigation.vue
@@ -0,0 +1,40 @@
+ <template>
+ <nav>
+        <ul>
+            <li><a href="home">Home</a></li>
+            <li><a href="addNewUser">Add New User</a></li>
+            <li><a href="listUsers">List All Users</a></li>
+        </ul>
+    </nav>
+</template>
+<script>
+export default
+{
+    name:'Navigation'
+}
+</script>
+<style scoped lang="scss">
+nav {
+    background-color: #333;
+    margin: 0;
+    overflow: hidden;
+    margin-bottom:5%;
+    ul{
+        margin: 0;
+        padding: 0;
+        li {
+                /* This allow us to arrange list items in a row, without using float */
+                display: inline-block;
+                list-style-type: none;
+                 a {
+                        color: #aaa;
+                        background-color:inherit;
+                        display: block;
+                        line-height: 2em;
+                        padding: 0.5em 0.5em;
+                        text-decoration: none;
+                    }
+            }
+    }
+}
+</style>
